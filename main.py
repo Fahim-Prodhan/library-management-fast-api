e97cdcd687d6fa5022fbbea2a42b4d7cfcfe025d
fahim-prodhan <fahimprodhan0@gmail.com>
2026-09-17 12:43:54 +0600

    allow all origin

--- main.py
+++ main.py
@@ -12,20 +12,11 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "http://127.0.0.1:5173",
-    "http://127.0.0.1:5174",
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    'https://steady-kelpie-5f1c9b.netlify.app'
-]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_origin_regex=r"http://(localhost|127\.0\.0\.1):\d+",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
